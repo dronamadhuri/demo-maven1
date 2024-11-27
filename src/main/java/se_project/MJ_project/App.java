1d7b90b77acca03659a4a4d9d750632144f59e3d
dronamadhuri <dadidronamadhuri@gmail.com>
2024-11-27 11:11:56 +0530

    Update App.java

--- src/main/java/se_project/MJ_project/App.java
+++ src/main/java/se_project/MJ_project/App.java
@@ -4,5 +4,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "hi i am Drona ,22BD1A664J,SE project" );
+        System.out.println( "hi i am Sreeja ,22BD1A665E" );
     }
 }
